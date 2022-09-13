59326c80ee0740206bd80b96e3e65e8bee31891a
relliikz <100636896+relliikz@users.noreply.github.com>
2022-09-13 13:52:03 +1000

    1 file modified

--- models/colours.enum.ts
+++ models/colours.enum.ts
@@ -6,3 +6,27 @@ export enum Colours {
 }
 
 // TODO: implement a ColoursHelper class as shown in BodyParts
+export class ColoursHelper {
+  static Colours[] = [
+    Colours.Red,
+    Colours.Blue,
+    Colours.Yellow,
+    Colours.Green
+
+  ];
+  constructor() {}
+
+  static get(key: string): Colours {
+    switch (key) {
+      case "Red":
+        return Colours.Red;
+      case "Blue":
+          return Colours.Blue;
+      case "Yellow":
+        return Colours.Yellow;
+      case "Green":
+        return Colours.Green;
+    }
+  }
+
+}
